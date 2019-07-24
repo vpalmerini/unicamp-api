34505907ec84cf152297f925cc9e12e125ee3985
vpalmerini <victorpalmerini@gmail.com>
2019-07-23 23:20:45 -0300

    Reorganize docker commands

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,11 @@ RUN mkdir /code
 WORKDIR /code
 
 COPY requirements.txt /code/
-
 RUN pip install -r requirements.txt
 
 COPY . /code/
+
+COPY docker-entrypoint.sh /docker-entrypoint.sh
+RUN chmod a+x /docker-entrypoint.sh
+
+ENTRYPOINT ["./docker-entrypoint.sh"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+create_superuser="
+import django
+django.setup()
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+try:
+    User.objects.create_superuser('$SUPERUSER_NAME', '$SUPERUSER_EMAIL', '$SUPERUSER_PASSWORD', first_name='admin', last_name='1')
+    print('Superuser \'$SUPERUSER_NAME\' created')
+except IntegrityError:
+    print('Superuser \'$SUPERUSER_NAME\' already exists')
+except Exception as e:
+    print(e)
+"
+create_superuser() {
+    if [ -z "$SUPERUSER_NAME" ] || [ -z "$SUPERUSER_EMAIL" ] || [ -z "$SUPERUSER_PASSWORD" ]; then
+        echo "Environment variables for database not set, not creating superuser."
+    else
+        echo "Creating superuser"
+        python -c "$create_superuser"
+    fi
+}
+
+echo "Making migrations"
+# Make database migrations
+python manage.py makemigrations
+
+echo "Running migrations"
+# Apply database migrations
+python manage.py migrate
+
+create_superuser
+
+# python "script pra pegar os dados do json e instanciar objetos no db"
+
+echo "Executing CMD"
+exec "$@"
