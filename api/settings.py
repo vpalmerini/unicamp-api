22c23627e21413acff31eae550403e6e2faf89ee
vpalmerini <victorpalmerini@gmail.com>
2019-07-26 16:30:53 -0300

    Add debugging tools

--- api/settings.py
+++ api/settings.py
@@ -11,7 +11,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -23,7 +22,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Django Apps
+    'scraping',
     'subjects',
+
+    # Third-Party Apps
+    'django_extensions',
+    'django_werkzeug',
 ]
 
 MIDDLEWARE = [
@@ -82,16 +86,20 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
