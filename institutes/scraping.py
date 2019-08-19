cc56c50694a9767990037472d39315a5cd2b9a6f
vpalmerini <victorpalmerini@gmail.com>
2019-08-18 23:16:45 -0300

    Remove link field from Institute model

--- institutes/scraping.py
+++ institutes/scraping.py
@@ -1,7 +1,11 @@
 from selenium import webdriver
 from api.utils import data_to_json
 
-url = 'https://www.dac.unicamp.br/portal/caderno-de-horarios/2019/1/S/G'
+year = '2019'
+graduation_level = 'G'
+semester = '1'
+url = 'https://www.dac.unicamp.br/portal/caderno-de-horarios/{}/{}/S/{}'.format(
+    year, semester, graduation_level)
 driver_path = '/usr/local/lib/chromedriver'
 
 driver = webdriver.Chrome(driver_path)
@@ -21,8 +25,6 @@ def get_institutes(url):
             '\n')[0]
         obj["name"] = institute.find_element_by_tag_name('a').text.split(
             '\n')[-1]
-        obj["link"] = institute.find_element_by_tag_name('a').get_attribute(
-            'href')
         data["institutes"].append(obj)
 
 
