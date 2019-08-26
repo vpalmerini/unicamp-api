3392d6603238b3e294ee94535dddd404529ba5e4
vpalmerini <victorpalmerini@gmail.com>
2019-08-26 17:31:14 -0300

    Add permission for authenticated users

--- api/permissions.py
+++ api/permissions.py
@@ -11,3 +11,15 @@ class IsSafeOrIsStaff(permissions.BasePermission):
             return True
         # otherwise, will allow only admin requests
         return request.user and request.user.is_staff
+
+
+class IsAuthenticatedOrIsStaff(permissions.BasePermission):
+    """
+    Allow only GET requests for authenticated users
+    """
+    def has_permission(self, request, view):
+        # allow GET requests
+        if request.method == 'GET' and request.user.is_authenticated:
+            return True
+        # otherwise, will allow only admin requests
+        return request.user and request.user.is_staff
