60ee030f09f7e1d9b9b495e954fc8a3eac27445a
vpalmerini <victorpalmerini@gmail.com>
2019-08-26 17:38:38 -0300

    Remove unecessary permissions

--- courses/views.py
+++ courses/views.py
@@ -2,10 +2,8 @@ from rest_framework import viewsets
 from .models import Course
 from . import serializers
 from rest_framework_extensions.mixins import NestedViewSetMixin
-from api.permissions import IsSafeOrIsStaff
 
 
 class CourseViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
-    permission_classes = (IsSafeOrIsStaff, )
     queryset = Course.objects.all()
     serializer_class = serializers.CourseSerializer
--- subjects/views.py
+++ subjects/views.py
@@ -2,10 +2,8 @@ from rest_framework import viewsets
 from .models import Subject
 from . import serializers
 from rest_framework_extensions.mixins import NestedViewSetMixin
-from api.permissions import IsSafeOrIsStaff
 
 
 class SubjectViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
-    permission_classes = (IsSafeOrIsStaff, )
     queryset = Subject.objects.all()
     serializer_class = serializers.SubjectSerializer
