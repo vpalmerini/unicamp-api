568270389c0d25db1df1393e33420c0d6cc2e8d8
vpalmerini <victorpalmerini@gmail.com>
2019-08-26 17:41:51 -0300

    Update Student serializers and permissions

--- students/serializers.py
+++ students/serializers.py
@@ -2,7 +2,19 @@ from rest_framework import serializers
 from .models import Student
 
 
-class StudentSerializer(serializers.ModelSerializer):
+class StudentDetailSerializer(serializers.ModelSerializer):
+    classes = serializers.SerializerMethodField()
+
+    def get_classes(self, obj):
+        from classes.serializers import ClassListSerializer
+        return ClassListSerializer(obj.classes, many=True, read_only=True).data
+
+    class Meta:
+        model = Student
+        fields = ('ra', 'name', 'email', 'course', 'classes')
+
+
+class StudentListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = ('ra', 'name', 'email', 'course')
--- students/views.py
+++ students/views.py
@@ -2,8 +2,14 @@ from rest_framework import viewsets
 from .models import Student
 from . import serializers
 from rest_framework_extensions.mixins import NestedViewSetMixin
+from api.permissions import IsAuthenticatedOrIsStaff
 
 
 class StudentViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return serializers.StudentListSerializer
+        return serializers.StudentDetailSerializer
+
+    permission_classes = (IsAuthenticatedOrIsStaff, )
     queryset = Student.objects.all()
-    serializer_class = serializers.StudentSerializer
