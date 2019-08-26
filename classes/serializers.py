2774828591965e86f521cfa286c151eea9274125
vpalmerini <victorpalmerini@gmail.com>
2019-08-26 17:35:24 -0300

    Update Class serializers and permissions

--- classes/serializers.py
+++ classes/serializers.py
@@ -1,11 +1,22 @@
 from rest_framework import serializers
 from .models import Class
-from students.serializers import StudentSerializer
+from students.serializers import StudentListSerializer
 
 
-class ClassSerializer(serializers.ModelSerializer):
+class ClassListSerializer(serializers.ModelSerializer):
     schedules = serializers.StringRelatedField(many=True, read_only=True)
-    students = StudentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Class
+        fields = [
+            'class_id', 'positions', 'enrolled', 'subject',
+            'course_reservation', 'schedules', 'professors'
+        ]
+
+
+class ClassDetailSerializer(serializers.ModelSerializer):
+    schedules = serializers.StringRelatedField(many=True, read_only=True)
+    students = StudentListSerializer(many=True, read_only=True)
     professors = serializers.StringRelatedField(many=True, read_only=True)
 
     class Meta:
@@ -16,10 +27,12 @@ class ClassSerializer(serializers.ModelSerializer):
         ]
 
 
-class ClassListSerializer(ClassSerializer):
+class ClassListStudenDetailSerializer(serializers.ModelSerializer):
+    """
+    Serializes fields to show in student-detail endpoint
+    """
+    schedules = serializers.StringRelatedField(many=True, read_only=True)
+
     class Meta:
         model = Class
-        fields = [
-            'class_id', 'positions', 'enrolled', 'subject',
-            'course_reservation', 'schedules', 'professors'
-        ]
+        fields = ['class_id', 'subject', 'schedules', 'professors']
--- classes/views.py
+++ classes/views.py
@@ -2,15 +2,13 @@ from rest_framework import viewsets
 from .models import Class
 from . import serializers
 from rest_framework_extensions.mixins import NestedViewSetMixin
-from api.permissions import IsSafeOrIsStaff
 
 
 class ClassViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     def get_serializer_class(self):
         if self.action == 'list':
             return serializers.ClassListSerializer
-        return serializers.ClassSerializer
+        return serializers.ClassDetailSerializer
 
-    permission_classes = (IsSafeOrIsStaff, )
     queryset = Class.objects.all()
     lookup_field = 'class_id'
