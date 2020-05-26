26c594801dd9d4ccfe301ca5efff5cb28edf31cd
vpalmerini <victorpalmerini@gmail.com>
2020-05-26 16:43:23 -0300

    ref: refactor courses and subjects endpoints

--- api/urls.py
+++ api/urls.py
@@ -6,4 +6,6 @@ base_api_path = 'api/v1'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('{}/institutes/'.format(base_api_path), include('institutes.urls')),
+    path('{}/courses/'.format(base_api_path), include('courses.urls')),
+    path('{}/subjects/'.format(base_api_path), include('subjects.urls')),
 ]
