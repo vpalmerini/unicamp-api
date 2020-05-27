1df257faad310c41cc9c63d34e2777d0d3b76d3e
vpalmerini <victorpalmerini@gmail.com>
2020-05-27 17:47:09 -0300

    ref: refactor endpoints for classes, professors and students

--- api/urls.py
+++ api/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('{}/institutes/'.format(base_api_path), include('institutes.urls')),
     path('{}/courses/'.format(base_api_path), include('courses.urls')),
     path('{}/subjects/'.format(base_api_path), include('subjects.urls')),
+    path('{}/classes/'.format(base_api_path), include('classes.urls')),
+    path('{}/professors/'.format(base_api_path), include('professors.urls')),
+    path('{}/students/'.format(base_api_path), include('students.urls')),
 ]
